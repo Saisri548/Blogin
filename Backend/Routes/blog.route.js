652f9828a30af8763da804abb9dbcfd7f3b054ef
Saisri548 <thotasaisri7@gmail.com>
2026-06-29 15:07:42 +0530

    Fixed Upload

--- Backend/Routes/blog.route.js
+++ Backend/Routes/blog.route.js
@@ -2,10 +2,11 @@ import express from "express";
 import { getAllBlogs,singleBlog,deletedBlog,updateBlog,createBlog } from "../Controllers/BlogContollers/blog.contoller.js";
 import { Router } from "express";
 import { authMiddleware } from "../middleware/middleware.js";
+import upload from "../middleware/upload.js";
 const Brouter=express.Router()
 Brouter.get("/",getAllBlogs)
 Brouter.get("/:id",singleBlog)
-Brouter.post("/",authMiddleware,createBlog)
+Brouter.post("/",authMiddleware,upload.single("coverImage"),createBlog)
 Brouter.put("/:id",authMiddleware,updateBlog)
 Brouter.delete("/:id",authMiddleware,deletedBlog)
 export default Brouter;
